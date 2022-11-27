d9a531033e26e0caa3109035850bdd50101e08aa
Binit Rupakheti <cruiserponder@protonmail.com>
2022-11-27 21:21:54 +0545

    fixed sign out bug

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,19 +3,20 @@ import { useRouter } from "next/router";
 import React from "react";
 
 const Header = () => {
-  const router = useRouter();
   const { data: sessionData } = useSession();
-
+  const handleClick = async () => {
+    await signOut({
+      redirect: true,
+      callbackUrl: "/",
+    });
+  };
   return (
     <nav className="flex w-full items-center justify-between bg-purple-700 py-3 px-4">
       <div>{sessionData?.user?.name}</div>
       <ul>
         <button
           className="rounded-full bg-white/10 px-8 py-3 font-semibold text-black no-underline transition hover:bg-white/20"
-          onClick={async () => {
-            await signOut();
-            await router.replace("/");
-          }}
+          onClick={handleClick}
         >
           Sign Out
         </button>
