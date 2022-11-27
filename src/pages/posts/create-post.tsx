65d8e269940681a617030bce4d6072f7283b0238
Binit Rupakheti <cruiserponder@protonmail.com>
2022-11-27 18:34:11 +0545

    create posts added

---
+++ src/pages/posts/create-post.tsx
@@ -0,0 +1,47 @@
+import { useSession } from "next-auth/react";
+import React, { useState } from "react";
+import { trpc } from "../../utils/trpc";
+
+const CreatePost = () => {
+  const { data: sessionData } = useSession();
+  const mutation = trpc.posts.createPost.useMutation();
+
+  const [post, setPost] = useState({
+    title: "",
+    body: "",
+  });
+
+  const handleClick = async () => {
+    if (!!post.title && !!post.body) {
+      await mutation.mutateAsync(post);
+    }
+  };
+  if (!sessionData) {
+    return <div>You are not logged in</div>;
+  }
+  return (
+    <div className="h-[100vh] w-[100vw] items-center justify-center bg-slate-700 ">
+      <label>Title of Post</label>
+      <input
+        type="text"
+        onChange={(e) =>
+          setPost((post) => ({ ...post, title: e.target.value }))
+        }
+      />
+      <label>Body of post</label>
+      <textarea
+        onChange={(e) => setPost((post) => ({ ...post, body: e.target.value }))}
+      />
+      <button onClick={handleClick} disabled={mutation.isLoading}>
+        Submit Post
+      </button>
+      {mutation.error && (
+        <p className="text-red-500">
+          Something went wrong! {mutation.error.message}
+        </p>
+      )}
+    </div>
+  );
+};
+
+export default CreatePost;
---
+++ src/server/trpc/router/posts.ts
@@ -0,0 +1,42 @@
+import { router, publicProcedure, protectedProcedure } from "../trpc";
+import { z } from "zod";
+import { TRPCError } from "@trpc/server";
+export const postsRouter = router({
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    return await ctx.prisma.post.findMany({
+      take: 15,
+    });
+  }),
+  createPost: protectedProcedure
+    .input(
+      z
+        .object({
+          title: z.string(),
+          body: z.string(),
+        })
+        .required()
+    )
+    .mutation(async ({ ctx, input }) => {
+      console.log(input);
+      if (!ctx.session.user.email) {
+        throw new TRPCError({
+          code: "UNAUTHORIZED",
+          message: "You are not authorized to access this request",
+        });
+      }
+      console.log(ctx.session.user);
+      try {
+        const post = await ctx.prisma.post.create({
+          data: {
+            body: input.body,
+            title: input.title,
+            userId: ctx.session.user.id,
+          },
+        });
+        return post;
+      } catch (error) {
+        console.log(error);
+        return null;
+      }
+    }),
+});
