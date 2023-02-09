37c3037365a99d10aca60eaf217cf17e7a56e9e1
Binit Rupakheti <crusierponder@protonmail.com>
2023-02-09 16:59:19 +0545

    feat : added comments to post page

--- src/pages/posts/[pid].tsx
+++ src/pages/posts/[pid].tsx
@@ -1,65 +1,114 @@
+import Layout from "@/components/Layout";
+import Post from "@/components/Post";
+import {
+  ChatBubbleBottomCenterIcon,
+  ShareIcon,
+} from "@heroicons/react/24/solid";
+import clsx from "clsx";
 import { useSession } from "next-auth/react";
+import Head from "next/head";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { Suspense, useState } from "react";
+import { ImArrowDown, ImArrowUp } from "react-icons/im";
 import { trpc } from "../../utils/trpc";
 
-const Post = () => {
+const SinglePost = () => {
+  const session = useSession();
+  const utils = trpc.useContext();
+  const { mutateAsync } = trpc.posts.addCommentToPost.useMutation();
   const router = useRouter();
   const { pid } = router.query;
-  const { data: session } = useSession();
-  console.log(session);
-  const { data: post, isLoading } = trpc.posts.getPostById.useQuery({
+  const [comment, setComment] = useState("");
+  const { data: post } = trpc.posts.getPostById.useQuery({
     id: pid as string,
   });
-
-  if (isLoading) return <div>Loading...</div>;
-  if (!post) return <div>No Post</div>;
-  if (!session?.user?.email)
-    return (
-      <div>
-        <div>Unauthorized</div>
-        <div>
-          Return to login
-          <button onClick={() => router.replace("/")}>Login</button>
-        </div>
-      </div>
-    );
   return (
     <>
-      <div className="flex flex-col items-center">
-        <div className="m-2 flex h-fit w-9/12 flex-row justify-between rounded-md bg-gray-300/80 p-4">
-          <div>
-            <Link href={"/posts/" + post?.id}>
-              <h3 className="text-2xl text-blue-600">{post?.title}</h3>
-            </Link>
-            <p className="text-base">{post?.body}</p>
-            <p className="text-xs">
-              Posted By
-              <Link className="text-blue-600" href={"/" + post?.userId}>
-                {" " + post?.user?.name}
-              </Link>
-            </p>
+      <Head>
+        <title>Reddit Clone | Nextjs</title>
+        <meta name="description" content="Front of the internet" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className="flex h-screen w-screen justify-center bg-gray-300">
+        <Layout isAuthed={!!session}>
+          <div className="flex w-6/12 flex-col items-center pt-20">
+            <Suspense fallback={<div>Loading...</div>}>
+              <div
+                key={post?.id}
+                className="my-4 flex w-full flex-row rounded-sm bg-white"
+              >
+                <div className="-z-1 flex flex-col items-center gap-y-1 bg-gray-100 p-2">
+                  <ImArrowUp className={clsx("h-6 w-6 text-gray-400")} />
+                  <span className="text-sm font-bold">Vote</span>
+                  <ImArrowDown className="h-6 w-6 text-gray-400 " />
+                </div>
+                <div className="flex-col px-4 py-2">
+                  <h6 className="text-xs font-normal text-gray-500">
+                    Posted by {session.data?.user?.name}
+                  </h6>
+                  <h3 className="text-lg font-semibold">{post?.title}</h3>
+                  <p className="pb-2 text-sm">{post?.body}</p>
+                  <div className="flex gap-x-5">
+                    <Link
+                      href={`/posts/${post?.id}`}
+                      className="flex items-center gap-x-1 rounded-sm px-2 py-1 hover:bg-gray-200"
+                    >
+                      <ChatBubbleBottomCenterIcon className="h-4 w-4" />
+                      <span className="prose-none">Comments</span>
+                    </Link>
+                    <button className="flex items-center gap-x-1 rounded-sm px-2 py-1 hover:bg-gray-200">
+                      <ShareIcon className="h-4 w-4" />
+                      <span className="prose-none">Share</span>
+                    </button>
+                  </div>
+                </div>
+              </div>
+              <div className="flex w-full flex-col items-end">
+                <textarea
+                  className="flex h-24 w-full resize-none bg-gray-200 p-4"
+                  value={comment}
+                  onChange={(e) => setComment(e.target.value)}
+                />
+                <div className="px-4 py-2">
+                  <button
+                    onClick={async () => {
+                      if (comment.length !== 0) {
+                        const thing = await mutateAsync({
+                          body: comment,
+                          postId: pid as string,
+                        });
+                        if (!!thing) {
+                          setComment("");
+                          utils.posts.getPostById.invalidate();
+                        }
+                      }
+                    }}
+                    className="rounded-md bg-orange-500 px-4 py-2 hover:bg-orange-400"
+                  >
+                    Comment
+                  </button>
+                </div>
+              </div>
+              <div className="w-full">
+                {post?.comments.map((comment) => (
+                  <div
+                    key={comment.id}
+                    className="my-2 flex-col bg-white px-4 py-2"
+                  >
+                    <h6 className="text-xs font-normal text-gray-500">
+                      Commented by {comment.User.name}
+                    </h6>
+                    <p className="text-sm">{comment.body}</p>
+                  </div>
+                ))}
+              </div>
+            </Suspense>
           </div>
-
-          <button
-            className="m-1 cursor-pointer bg-purple-500 px-6 py-2 disabled:bg-gray-300"
-            disabled={post.userId !== session.user.id}
-          >
-            Edit
-          </button>
-        </div>
-        <button
-          className="py-2s m-1 cursor-pointer bg-purple-500 px-6 text-white"
-          onClick={async () => {
-            await router.replace("/posts");
-          }}
-        >
-          Back to posts
-        </button>
-      </div>
-      <div>Comments</div>
+        </Layout>
+      </main>
     </>
   );
 };
 
-export default Post;
+export default SinglePost;
