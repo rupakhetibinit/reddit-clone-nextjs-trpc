e672679f097e680a8b613bef293907bced1b0476
Binit Rupakheti <crusierponder@protonmail.com>
2023-02-11 10:23:59 +0545

    test oauth again

--- src/env/schema.mjs
+++ src/env/schema.mjs
@@ -23,6 +23,7 @@ export const serverSchema = z.object({
   DISCORD_CLIENT_SECRET: z.string(),
   GOOGLE_CLIENT_ID: z.string(),
   GOOGLE_CLIENT_SECRET: z.string(),
+  SECRET_KEY: z.string(),
 });
 
 /**
